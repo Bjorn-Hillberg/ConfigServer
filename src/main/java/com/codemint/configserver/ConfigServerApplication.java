59c434d11bfc08b7fb5af4e581bcd11a9e61d8eb
Björn Hillberg <Bjorn.hillberg@codemint.com>
2024-02-01 14:42:10 +0100

    first setup 20 minutes

---
+++ src/main/java/com/codemint/configserver/ConfigServerApplication.java
@@ -0,0 +1,38 @@
+package com.codemint.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+    @Controller
+    @ResponseBody
+    class ConfigurationHttpController {
+        private final ConfigurationRepository configurationRepository;
+
+        ConfigurationHttpController(ConfigurationRepository configurationRepository) {
+            this.configurationRepository = configurationRepository;
+        }
+
+        @GetMapping("/configurations")
+        Iterable<Configuration> configurations(Iterable<String> ids) {
+            return this.configurationRepository.findAllById(ids);
+        }
+    }
+
+    interface ConfigurationRepository extends CrudRepository<Configuration, String> {
+    }
+
+    record Configuration(@Id String Name, String configuration) {
+    }
+}
---
+++ src/test/java/com/codemint/configserver/TestServiceApplication.java
@@ -0,0 +1,15 @@
+package com.codemint.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(ConfigServerApplication::main)
+                .with(TestServiceApplication.class)
+                .run(args);
+    }
+}
