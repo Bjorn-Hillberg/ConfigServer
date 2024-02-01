881a7626a62f3b6020990683ca1a1f97ddb5335f
Björn Hillberg <Bjorn.hillberg@codemint.com>
2024-02-01 14:53:19 +0100

    first setup 20 minutes

--- src/test/java/com/codemint/configserver/TestServiceApplication.java
+++ src/test/java/com/codemint/configserver/TestServiceApplication.java
@@ -1,7 +1,6 @@
 package com.codemint.configserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
